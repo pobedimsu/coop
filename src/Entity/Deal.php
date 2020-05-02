ab4f1df10bf5d574bf2ace7dc62f9b31e22c8ff2
artem <digital_boy@mail.ru>
2020-05-02 08:00:32 +0700

    fix

--- src/Entity/Deal.php
+++ src/Entity/Deal.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Doctrine\StatusTrait;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Smart\CoreBundle\Doctrine\ColumnTrait;
 
@@ -24,6 +26,8 @@ class Deal
     use ColumnTrait\CreatedAt;
     use ColumnTrait\UpdatedAt;
     use ColumnTrait\Comment;
+
+    use StatusTrait;
 
     const STATUS_NEW                    = 0;
     const STATUS_VIEW                   = 1;
@@ -81,13 +85,6 @@ class Deal
     protected $quantity;
 
     /**
-     * @var int
-     *
-     * @ORM\Column(type="smallint", nullable=false, options={"unsigned"=true, "default":0})
-     */
-    protected $status;
-
-    /**
      * Дата просмотра
      *
      * @var \DateTime
@@ -131,9 +128,6 @@ class Deal
      */
     protected $transactions;
 
-    /**
-     * Deal constructor.
-     */
     public function __construct()
     {
         $this->created_at   = new \DateTime();
@@ -149,79 +143,11 @@ class Deal
         $this->updated_at = new \DateTime();
     }
 
-    /**
-     * @return array
-     */
-    static public function getStatusFormChoices(): array
-    {
-        return array_flip(self::$status_values);
-    }
-
-    /**
-     * @return array
-     */
-    static public function getStatusValues(): array
-    {
-        return self::$status_values;
-    }
-
-    /**
-     * @return bool
-     */
-    static public function isStatusExist($status): bool
-    {
-        if (isset(self::$status_values[$status])) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStatusAsText(): string
-    {
-        if (isset(self::$status_values[$this->status])) {
-            return self::$status_values[$this->status];
-        }
-
-        return 'N/A';
-    }
-
-    /**
-     * @return int
-     */
-    public function getStatus(): int
-    {
-        return $this->status;
-    }
-
-    /**
-     * @param int $status
-     *
-     * @return $this
-     */
-    public function setStatus($status): Deal
-    {
-        $this->status = $status;
-
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
     public function getCost(): int
     {
         return $this->cost;
     }
 
-    /**
-     * @param int $cost
-     *
-     * @return $this
-     */
     public function setCost($cost): Deal
     {
         $this->cost = $cost;
@@ -229,19 +155,11 @@ class Deal
         return $this;
     }
 
-    /**
-     * @return int
-     */
     public function getActualCost(): int
     {
         return $this->actual_cost;
     }
 
-    /**
-     * @param int $actual_cost
-     *
-     * @return $this
-     */
     public function setActualCost($actual_cost): self
     {
         $this->actual_cost = $actual_cost;
@@ -249,19 +167,11 @@ class Deal
         return $this;
     }
 
-    /**
-     * @return int
-     */
     public function getAmountCost(): int
     {
         return $this->amount_cost;
     }
 
-    /**
-     * @param int $amount_cost
-     *
-     * @return $this
-     */
     public function setAmountCost($amount_cost): self
     {
         $this->amount_cost = $amount_cost;
@@ -269,19 +179,11 @@ class Deal
         return $this;
     }
 
-    /**
-     * @return int
-     */
-    public function getQuantity(): ?int
+    public function getQuantity(): int
     {
-        return $this->quantity;
+        return (int) $this->quantity;
     }
 
-    /**
-     * @param int $quantity
-     *
-     * @return $this
-     */
     public function setQuantity($quantity): self
     {
         $this->quantity = $quantity;
@@ -289,19 +191,11 @@ class Deal
         return $this;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getViewedAt(): ?\DateTime
     {
         return $this->viewed_at;
     }
 
-    /**
-     * @param \DateTime $viewed_at
-     *
-     * @return $this
-     */
     public function setViewedAt($viewed_at): self
     {
         $this->viewed_at = $viewed_at;
@@ -309,19 +203,11 @@ class Deal
         return $this;
     }
 
-    /**
-     * @return Offer
-     */
     public function getOffer(): Offer
     {
         return $this->offer;
     }
 
-    /**
-     * @param Offer $offer
-     *
-     * @return $this
-     */
     public function setOffer(Offer $offer): self
     {
         $this->offer = $offer;
@@ -329,19 +215,11 @@ class Deal
         return $this;
     }
 
-    /**
-     * @return User
-     */
     public function getContractorUser(): User
     {
         return $this->contractor_user;
     }
 
-    /**
-     * @param User $contractor_user
-     *
-     * @return $this
-     */
     public function setContractorUser(User $contractor_user): self
     {
         $this->contractor_user = $contractor_user;
@@ -349,19 +227,11 @@ class Deal
         return $this;
     }
 
-    /**
-     * @return User
-     */
     public function getDeclarantUser(): User
     {
         return $this->declarant_user;
     }
 
-    /**
-     * @param User $declarant_user
-     *
-     * @return $this
-     */
     public function setDeclarantUser(User $declarant_user): self
     {
         $this->declarant_user = $declarant_user;
@@ -370,7 +240,7 @@ class Deal
     }
 
     /**
-     * @return Transaction[]|ArrayCollection
+     * @return Transaction[]|Collection
      */
     public function getTransactions()
     {
@@ -378,7 +248,7 @@ class Deal
     }
 
     /**
-     * @param Transaction[]|ArrayCollection $transactions
+     * @param Transaction[]|Collection $transactions
      *
      * @return $this
      */
