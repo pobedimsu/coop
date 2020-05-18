edb8db5dbc7f2b6210c208bcd4627fc71d182360
artem <artem.digi@gmail.com>
2020-05-18 14:19:03 +0700

    custom localized date

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -9,7 +9,9 @@ use App\Entity\Offer;
 use App\Entity\User;
 use App\Service\BillService;
 use Doctrine\ORM\EntityManagerInterface;
+use Twig\Environment;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
@@ -46,6 +48,61 @@ class AppExtension extends AbstractExtension
 
             new TwigFunction('app_tg_bot_name',    [$this, 'getTgBotName']),
         ];
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('app_date', [$this, 'getDate'], ['needs_environment' => true]),
+        ];
+    }
+
+    /**
+     * http://userguide.icu-project.org/formatparse/datetime
+     */
+    public function getDate(
+        Environment $env,
+        $date,
+        $format = 'd MMMM Y г., HH:mm',
+        $dateFormat = 'medium',
+        $timeFormat = 'medium',
+        $locale = null,
+        $timezone = null,
+        $calendar = 'gregorian'
+    ): string {
+        $date = twig_date_converter($env, $date, $timezone);
+
+        $formatValues = [
+            'none' => \IntlDateFormatter::NONE,
+            'short' => \IntlDateFormatter::SHORT,
+            'medium' => \IntlDateFormatter::MEDIUM,
+            'long' => \IntlDateFormatter::LONG,
+            'full' => \IntlDateFormatter::FULL,
+        ];
+
+        if (PHP_VERSION_ID < 50500 || !class_exists('IntlTimeZone')) {
+            $formatter = \IntlDateFormatter::create(
+                $locale,
+                $formatValues[$dateFormat],
+                $formatValues[$timeFormat],
+                $date->getTimezone()->getName(),
+                'gregorian' === $calendar ? \IntlDateFormatter::GREGORIAN : \IntlDateFormatter::TRADITIONAL,
+                $format
+            );
+
+            return $formatter->format($date->getTimestamp());
+        }
+
+        $formatter = \IntlDateFormatter::create(
+            $locale,
+            $formatValues[$dateFormat],
+            $formatValues[$timeFormat],
+            \IntlTimeZone::createTimeZone($date->getTimezone()->getName()),
+            'gregorian' === $calendar ? \IntlDateFormatter::GREGORIAN : \IntlDateFormatter::TRADITIONAL,
+            $format
+        );
+
+        return $formatter->format($date->getTimestamp());
     }
 
     /**
